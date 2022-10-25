7e808700d815d91711a4813e8bcb7344e7a4e68a
Coderwsq <9699507+wsq__666@user.noreply.gitee.com>
2022-10-25 17:12:31 +0800

    '环境搭建和实现收集依赖'

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,39 @@
+class ReactiveEffect {
+  private _fn: any
+
+  constructor(fn) {
+    this._fn = fn
+  }
+
+  run() {
+    activeEffect = this
+    this._fn()
+  }
+}
+
+const targetMap = new Map()
+export function track(target, key) {
+  let depsMap = targetMap.get(target)
+  if(!depsMap) {
+    depsMap = new Map()
+    targetMap.set(target, depsMap)
+  }
+
+  let dep = depsMap.get(key)
+  if(!dep) {
+    dep = new Map()
+    depsMap.set(key, dep)
+  }
+  dep.add(activeEffect)
+}
+
+export function trigger() {
+  
+}
+
+let activeEffect
+export function effect(fn) {
+  const _effect = new ReactiveEffect(fn)
+
+  _effect.run()
+}

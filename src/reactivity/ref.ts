31ee63ec25b56fea35dea73aee5fa3edc09b0087
Coderwsq <9699507+wsq__666@user.noreply.gitee.com>
2022-11-02 23:32:32 +0800

    '实现ref、isRef、unRef、proxyRefs'

---
+++ src/reactivity/ref.ts
@@ -0,0 +1,66 @@
+import { trackEffects, triggerEffects, isTracking } from './effect'
+import { hasChanged, isObject } from '../shared'
+import { reactive } from './reactive'
+
+class RefImpl {
+  private _value: any
+  public dep
+  private _rawValue: any
+  public __v_isRef = true
+
+  constructor(value) {
+    // 如果value是一个对象 那就用reative进行包裹
+    this._value = convert(value)
+    this.dep = new Set()
+  }
+
+  get value() {
+    trackRefValue(this)
+    return this._value
+  }
+
+  set value(newValue) {
+    if (hasChanged(newValue, this._value)) {
+      this._rawValue = newValue
+      this._value = convert(newValue)
+      triggerEffects(this.dep)
+    }
+  }
+}
+
+function convert(value) {
+  return isObject(value) ? reactive(value) : value
+}
+
+function trackRefValue(ref) {
+  if (isTracking()) {
+    trackEffects(ref.dep)
+  }
+}
+
+export function ref(value) {
+  return new RefImpl(value)
+}
+
+export function isRef(ref) {
+  return !!ref.__v_isRef
+}
+
+export function unRef(ref) {
+  return isRef(ref) ? ref.value : ref
+}
+
+export function proxyRefs(objectwithRefs) {
+  return new Proxy(objectwithRefs, {
+    get(target, key) {
+      return unRef(Reflect.get(target, key))
+    },
+    set(target, key, value) {
+      if (isRef(target[key]) && !isRef(value)) {
+        return target[key].value = value
+      } else {
+        return Reflect.set(target, key, value)
+      }
+    }
+  })
+}
